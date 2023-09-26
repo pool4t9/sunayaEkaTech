6be16259664e82a07db1804f98fa27f7666c6fb9
pool4t9 <gulshan.gupta@omlogic.co.in>
2023-09-26 18:28:55 +0530

    changes in forms

--- web/src/helper.js
+++
@@ -1,8 +0,0 @@
-export const validate = (initialValue) => {
-  const error = {};
-  Object.entries(initialValue).map(([key, value]) => {
-    if (value.length) error[key] = false;
-    else error[`${key}Error`] = true;
-  });
-  return error;
-};
--- web/src/pages/Register.jsx
+++ web/src/pages/Register.jsx
@@ -17,6 +17,8 @@ import axios from "../axios";
 import { Link, useNavigate } from "react-router-dom";
 import { useForm } from "react-hook-form";
 import useFetch from "../hooks/useFetch";
+import Loader from "../components/Loader";
+import AlertBanner from "../components/AlertBanner";
 
 // eslint-disable-next-line react/prop-types
 const Form2 = ({ setStep }) => {
@@ -25,8 +27,6 @@ const Form2 = ({ setStep }) => {
   const [profile, setProfile] = useState("");
 
   let { loading, error, fetchedData } = useFetch("/api/user/get-profile");
-
-  useEffect(() => {}, []);
 
   const {
     handleSubmit,
@@ -53,8 +53,11 @@ const Form2 = ({ setStep }) => {
     setValue("last_name", fetchedData?.profile?.last_name);
   }, [fetchedData, setValue]);
 
-  if (loading) return <div>Loading</div>;
-  if (error) return <div>{error}</div>;
+  if (loading) return <Loader />;
+  if (error)
+    return (
+      <AlertBanner status={"error"} message={error} title={"Server Error"} />
+    );
 
   const submitHandler = async (values) => {
     const { contact, dob, gender, qualification } = values;
@@ -74,16 +77,6 @@ const Form2 = ({ setStep }) => {
         duration: 3000,
         isClosable: true,
       });
-      let updatedUser = {
-        ...user,
-        contact,
-        dob,
-        gender,
-        qualification,
-        profile,
-      };
-      console.log(updatedUser);
-      localStorage.setItem("user", JSON.stringify(updatedUser));
       localStorage.setItem("step", 3);
       window.location = "/profile";
       return;
@@ -136,169 +129,182 @@ const Form2 = ({ setStep }) => {
       <Heading w="100%" textAlign={"center"} fontWeight="normal" mb="2%">
         Update Your Details
       </Heading>
-      <Flex mt={"2%"}>
-        <FormControl mr="5%">
-          <FormLabel htmlFor="first-name" fontWeight={"normal"}>
-            First name
-          </FormLabel>
-          <Input
-            id="first-name"
-            placeholder="First name"
-            name="first_name"
-            isDisabled={true}
-            {...register("first_name")}
-          />
-        </FormControl>
-
-        <FormControl>
-          <FormLabel htmlFor="last-name" fontWeight={"normal"}>
-            Last name
-          </FormLabel>
-          <Input
-            id="last-name"
-            placeholder="Last name"
-            name="last_name"
-            {...register("last_name")}
-            isDisabled={true}
-          />
-        </FormControl>
-      </Flex>
-      <Flex mt={"2%"}>
-        <FormControl mr="5%" isInvalid={errors.contact}>
-          <FormLabel htmlFor="contact" fontWeight={"normal"}>
-            Contact
-          </FormLabel>
-          <Input
-            type="text"
-            id="contact"
-            placeholder="Contact Number"
-            name="contact"
-            {...register("contact", {
-              required: "contact is required",
-            })}
-          />
-          <FormErrorMessage>
-            {errors.contact && errors.contact.message}
-          </FormErrorMessage>
-        </FormControl>
-        <FormControl isInvalid={errors.dob}>
-          <FormLabel htmlFor="dob" fontWeight={"normal"}>
-            DOB
-          </FormLabel>
-          <Input
-            type="date"
-            id="dob"
-            name="dob"
-            {...register("dob", {
-              required: "dob is required",
-            })}
-          />
-          <FormErrorMessage>
-            {errors.dob && errors.dob.message}
-          </FormErrorMessage>
-        </FormControl>
-      </Flex>
-
-      <Flex mt={"2%"}>
-        <FormControl
-          className="form-floating"
-          mr={"5%"}
-          isInvalid={errors.gender}
-        >
-          <FormLabel
-            htmlFor="gender"
-            fontSize="sm"
-            fontWeight="sm"
-            color="gray.700"
-            _dark={{
-              color: "gray.50",
-            }}
-          >
-            Gender
-          </FormLabel>
-          <Select
-            placeholder="Select Gender"
-            id="gender"
-            focusBorderColor="brand.400"
-            shadow="sm"
-            size="sm"
-            w="full"
-            rounded="md"
-            {...register("gender", {
-              required: "Gender is required",
-            })}
-          >
-            <option value="male">Male</option>
-            <option value="female">Female</option>
-            <option value="other">Other</option>
-          </Select>
-          <FormErrorMessage>
-            {errors.gender && errors.gender.message}
-          </FormErrorMessage>
-        </FormControl>
-        <FormControl className="form-floating" isInvalid={errors.qualification}>
-          <FormLabel
-            htmlFor="qualification"
-            fontSize="sm"
-            fontWeight="md"
-            color="gray.700"
-            _dark={{
-              color: "gray.50",
-            }}
-          >
-            Highest Qualification
-          </FormLabel>
-          <Select
-            placeholder="Select Qualification"
-            // selected={qualification}
-            // onChange={changeHandler}
-            name="qualification"
-            id="qualification"
-            focusBorderColor="brand.400"
-            shadow="sm"
-            size="sm"
-            w="full"
-            rounded="md"
-            {...register("qualification", {
-              required: "qualification is required",
-            })}
-          >
-            <option value="">All</option>
-            <option value="option1">Option 1</option>
-            <option value="option2">Option 2</option>
-            <option value="option3">Option 3</option>
-          </Select>
-          <FormErrorMessage>
-            {errors.qualification && errors.qualification.message}
-          </FormErrorMessage>
-        </FormControl>
-      </Flex>
-      <FormControl isInvalid={errors.profile}>
-        <FormLabel htmlFor="profile" fontWeight={"normal"}>
-          Profile
-        </FormLabel>
-        <input
-          type="file"
-          id="profile"
-          name="profile"
-          accept="image/png, image/jpeg"
-          onChange={uploadImage}
-        />
-        <FormErrorMessage>
-          {errors.profile && errors.profile.message}
-        </FormErrorMessage>
-      </FormControl>
-      <Button
-        colorScheme="teal"
-        variant="outline"
-        w={"7rem"}
-        mt={"3%"}
-        onClick={handleSubmit(submitHandler)}
-        isLoading={isSubmitting}
-        loadingText="Submitting"
+      <Box
+        borderWidth="1px"
+        rounded="lg"
+        shadow="1px 1px 3px rgba(0,0,0,0.3)"
+        maxWidth={500}
+        p={6}
+        m="10px auto"
+        as="form"
+        onSubmit={handleSubmit(submitHandler)}
       >
-        Submit
-      </Button>
+        <Flex mt={"2%"}>
+          <FormControl mr="5%">
+            <FormLabel htmlFor="first-name" fontWeight={"normal"}>
+              First name
+            </FormLabel>
+            <Input
+              id="first-name"
+              placeholder="First name"
+              name="first_name"
+              isDisabled={true}
+              {...register("first_name")}
+            />
+          </FormControl>
+
+          <FormControl>
+            <FormLabel htmlFor="last-name" fontWeight={"normal"}>
+              Last name
+            </FormLabel>
+            <Input
+              id="last-name"
+              placeholder="Last name"
+              name="last_name"
+              {...register("last_name")}
+              isDisabled={true}
+            />
+          </FormControl>
+        </Flex>
+        <Flex mt={"2%"}>
+          <FormControl mr="5%" isInvalid={errors.contact}>
+            <FormLabel htmlFor="contact" fontWeight={"normal"}>
+              Contact
+            </FormLabel>
+            <Input
+              type="text"
+              id="contact"
+              placeholder="Contact Number"
+              name="contact"
+              {...register("contact", {
+                required: "contact is required",
+              })}
+            />
+            <FormErrorMessage>
+              {errors.contact && errors.contact.message}
+            </FormErrorMessage>
+          </FormControl>
+          <FormControl isInvalid={errors.dob}>
+            <FormLabel htmlFor="dob" fontWeight={"normal"}>
+              DOB
+            </FormLabel>
+            <Input
+              type="date"
+              id="dob"
+              name="dob"
+              {...register("dob", {
+                required: "dob is required",
+              })}
+            />
+            <FormErrorMessage>
+              {errors.dob && errors.dob.message}
+            </FormErrorMessage>
+          </FormControl>
+        </Flex>
+
+        <Flex mt={"2%"}>
+          <FormControl
+            className="form-floating"
+            mr={"5%"}
+            isInvalid={errors.gender}
+          >
+            <FormLabel
+              htmlFor="gender"
+              fontSize="sm"
+              fontWeight="sm"
+              color="gray.700"
+              _dark={{
+                color: "gray.50",
+              }}
+            >
+              Gender
+            </FormLabel>
+            <Select
+              placeholder="Select Gender"
+              id="gender"
+              focusBorderColor="brand.400"
+              shadow="sm"
+              size="sm"
+              w="full"
+              rounded="md"
+              {...register("gender", {
+                required: "Gender is required",
+              })}
+            >
+              <option value="male">Male</option>
+              <option value="female">Female</option>
+              <option value="other">Other</option>
+            </Select>
+            <FormErrorMessage>
+              {errors.gender && errors.gender.message}
+            </FormErrorMessage>
+          </FormControl>
+          <FormControl
+            className="form-floating"
+            isInvalid={errors.qualification}
+          >
+            <FormLabel
+              htmlFor="qualification"
+              fontSize="sm"
+              fontWeight="md"
+              color="gray.700"
+              _dark={{
+                color: "gray.50",
+              }}
+            >
+              Highest Qualification
+            </FormLabel>
+            <Select
+              placeholder="Select Qualification"
+              // selected={qualification}
+              // onChange={changeHandler}
+              name="qualification"
+              id="qualification"
+              focusBorderColor="brand.400"
+              shadow="sm"
+              size="sm"
+              w="full"
+              rounded="md"
+              {...register("qualification", {
+                required: "qualification is required",
+              })}
+            >
+              <option value="">All</option>
+              <option value="option1">Option 1</option>
+              <option value="option2">Option 2</option>
+              <option value="option3">Option 3</option>
+            </Select>
+            <FormErrorMessage>
+              {errors.qualification && errors.qualification.message}
+            </FormErrorMessage>
+          </FormControl>
+        </Flex>
+        <FormControl isInvalid={errors.profile}>
+          <FormLabel htmlFor="profile" fontWeight={"normal"}>
+            Profile
+          </FormLabel>
+          <input
+            type="file"
+            id="profile"
+            name="profile"
+            accept="image/png, image/jpeg"
+            onChange={uploadImage}
+          />
+          <FormErrorMessage>
+            {errors.profile && errors.profile.message}
+          </FormErrorMessage>
+        </FormControl>
+        <Button
+          colorScheme="teal"
+          variant="outline"
+          w={"7rem"}
+          mt={"3%"}
+          isLoading={isSubmitting}
+          loadingText="Submitting"
+        >
+          Submit
+        </Button>
+      </Box>
     </>
   );
 };
